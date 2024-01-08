e2bdf8f7321f11927994cadcb754d379c8d1b7e4
Alexandre Espinosa Menor <aemenor@gmail.com>
2024-01-08 17:50:08 +0100

    rutas correxidas

--- systemd/add_api_token_to_env_systemd.sh
+++ systemd/add_api_token_to_env_systemd.sh
@@ -1,14 +1,14 @@
-# https://t.me/CIGdhBot
-BOT_TG_CIGDH_APITOKEN=xxx:xxxxx
+# https://t.me/CIGdinahostingBot
+BOT_TG_CIGDH_APITOKEN=xxxx:xxx
 
-BOT_TG_CIGDH_CANLE_PROBAS_DEFAULT=@xxxxx
+BOT_TG_CIGDH_CANLE_PROBAS_DEFAULT=@xxx
 
 # id de canle por defecto, podese sobreescribir com parámetro "-chatID -xxxxxxxxxx"
 # recolhido via https://web.telegram.org/ daba um chatID distinto
 BOT_TG_CIGDH_CANLE_ID_DEFAULT=-xxxxxx
 
 # se empregamos de provider Google Cloud. Olho! fullpath
-CREDENTIALS_FILE_ACCOUNT_SERVICE_GCP=/home/pi/FP_SAN_CLEMENTE_proxecto_DAM_distancia/service-account.json
+CREDENTIALS_FILE_ACCOUNT_SERVICE_GCP=/home/pi/CIGdinahostingBot/service-account.json
 
 # yaml config de usuarios, tamem fullpath
-FILE_CONFIG_USERS=/home/pi/FP_SAN_CLEMENTE_proxecto_DAM_distancia/config.yml
+FILE_CONFIG_USERS=/home/pi/CIGdinahostingBot/config.yml
